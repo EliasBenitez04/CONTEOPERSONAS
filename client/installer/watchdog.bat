b1be487ba3e551e7622ebab787e2c64f50ca4e5a
EliasBenitez04 <119250215+EliasBenitez04@users.noreply.github.com>
2026-09-14 16:07:45 -0300

    Eliminar watchdog BAT legado del instalador

--- client/installer/watchdog.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-setlocal
-cd /d "%~dp0"
-
-:restart
-start /wait "ContePersonas" "%~dp0ContePersonas.exe"
-echo [CLIENT] ContePersonas se cerro. Reinicio en 10 segundos.
-timeout /t 10 /nobreak >nul
-goto restart
