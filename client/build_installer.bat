3872794c3f29c03ecf0b7ef94e4e64944e73d838
EliasBenitez04 <119250215+EliasBenitez04@users.noreply.github.com>
2026-09-13 22:50:27 -0300

    Corregir deteccion de Inno Setup en rutas con parentesis

--- client/build_installer.bat
+++ client/build_installer.bat
@@ -3,38 +3,38 @@ setlocal
 cd /d "%~dp0"
 
 call build_client.bat
-if errorlevel 1 exit /b 1
+if errorlevel 1 goto :build_error
 
 set "ISCC="
 
 if exist "%LOCALAPPDATA%\Programs\Inno Setup 6\ISCC.exe" set "ISCC=%LOCALAPPDATA%\Programs\Inno Setup 6\ISCC.exe"
-if exist "C:\Program Files (x86)\Inno Setup 6\ISCC.exe" set "ISCC=C:\Program Files (x86)\Inno Setup 6\ISCC.exe"
-if exist "C:\Program Files\Inno Setup 6\ISCC.exe" set "ISCC=C:\Program Files\Inno Setup 6\ISCC.exe"
+if exist "%ProgramFiles(x86)%\Inno Setup 6\ISCC.exe" set "ISCC=%ProgramFiles(x86)%\Inno Setup 6\ISCC.exe"
+if exist "%ProgramFiles%\Inno Setup 6\ISCC.exe" set "ISCC=%ProgramFiles%\Inno Setup 6\ISCC.exe"
 
-if not defined ISCC (
-    for /f "delims=" %%I in ('where ISCC.exe 2^>nul') do (
-        set "ISCC=%%I"
-        goto :found_iscc
-    )
-)
+if defined ISCC goto :found_iscc
+
+for /f "delims=" %%I in ('where.exe ISCC.exe 2^>nul') do set "ISCC=%%I" & goto :found_iscc
+
+goto :not_found
 
 :found_iscc
-if not defined ISCC (
-    echo [INSTALLER] Inno Setup 6 no encontrado.
-    echo [INSTALLER] Rutas revisadas:
-    echo   %LOCALAPPDATA%\Programs\Inno Setup 6\ISCC.exe
-    echo   C:\Program Files (x86)\Inno Setup 6\ISCC.exe
-    echo   C:\Program Files\Inno Setup 6\ISCC.exe
-    echo   PATH de Windows
-    exit /b 1
-)
-
 echo [INSTALLER] Usando: %ISCC%
 "%ISCC%" "installer\ContePersonas.iss"
-if errorlevel 1 (
-    echo [INSTALLER] Error compilando instalador.
-    exit /b 1
-)
+if errorlevel 1 goto :compile_error
 
 echo [INSTALLER] Listo: client\dist\installer\SetupContePersonas.exe
-endlocal
+exit /b 0
+
+:not_found
+echo [INSTALLER] Inno Setup 6 no encontrado.
+echo [INSTALLER] Se reviso LOCALAPPDATA, Program Files y PATH de Windows.
+echo [INSTALLER] Verifica con: where.exe ISCC.exe
+exit /b 1
+
+:compile_error
+echo [INSTALLER] Error compilando instalador.
+exit /b 1
+
+:build_error
+echo [BUILD] Error generando ContePersonas.
+exit /b 1
